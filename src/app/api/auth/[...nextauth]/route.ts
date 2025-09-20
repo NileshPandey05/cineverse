d669cba23cf2aabfa2b57436aefdb20d5b540413
Nilesh Pandey <nileshpandey_ce_2021@ltce.in>
2025-09-20 23:23:45 +0530

    solve type error

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,5 @@
-import { AuthOption } from "@/lib/auth"
-import NextAuth from "next-auth"
+import NextAuth from "next-auth";
+import { AuthOption } from "@/lib/auth"; // adjust path as needed
 
-const handler = NextAuth(AuthOption)
-
-export { handler as GET, handler as POST}
+const handler = NextAuth(AuthOption);
+export { handler as GET, handler as POST };
